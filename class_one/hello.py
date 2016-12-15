eb8300f21b1007e4af0043d28c2f9904045fe814
MYHITGITBIT <xuehanqing@winddeMacBook-Pro.local>
2016-12-15 16:56:36 +0800

    Add post error remind

--- class_one/hello.py
+++ class_one/hello.py
@@ -1,3 +1,5 @@
+import textwrap
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -11,9 +13,25 @@ class IndexHandler(tornado.web.RequestHandler):
         greeting = self.get_argument('greeting', 'Hello')
         self.write(greeting + ', friendly user!\n')
 
+class ReverseHandler(tornado.web.RequestHandler):
+	def get(self, inputs):
+		self.write( inputs[::-1])
+
+class WrapHandler(tornado.web.RequestHandler):
+	def post(self):
+		text  = self.get_argument('text')
+		width = self.get_argument('width', 40)
+		self.write(textwrap.fill(text, int(width)))
+
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    app = tornado.web.Application(
+    	handlers=[
+    		#(r"/reverse/(\W+)", ReverseHandler),
+    		#(r"/wrap/", WrapHandler),
+    		(r"/", IndexHandler)
+    	]
+    )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
