43ed14282248a848b2d5066eafb60b2f04d0debe
MYHITGITBIT <1120310228@hit.edu.cn>
2016-12-31 23:02:45 +0800

    Add splider to exact gene info（Hello 2017）

---
+++ handleJson.py
@@ -0,0 +1,44 @@
+#coding=utf-8
+import json
+
+class HandleJson(object):
+	"""docstring for HandleJson"""
+	def __init__(self, fileName):
+		super(HandleJson, self).__init__()
+		self.fileName = fileName
+
+	def run(self):
+		netfile = open(self.fileName)
+		try:
+			'''
+			Read the json file from disk
+			'''
+			net = netfile.read()
+			netParsed = json.loads(net)
+			nodes = set()
+			'''
+			Label edges
+			'''
+			for elms in netParsed:
+				'''
+				Lable the edges
+				'''
+				if elms['group'] == 'edges':
+					edgeType =  elms['data']['type']
+					elms['classes'] = 1 if edgeType == 'controls-state-change-of' else 0
+				else:
+					nodeType = elms['data']['importance']
+					elms['classes'] = nodeType
+			'''
+			save the new json 
+			'''
+			labelNet = open('net_new.json', 'w')
+			json.dump(netParsed, labelNet)
+		finally:
+			netfile.close()
+
+if __name__ == "__main__":
+	netHandle = HandleJson('net.json')
+	netHandle.run()
+	pass
+
