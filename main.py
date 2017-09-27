190f05e563c3866e4fb097f1a816c315cee6bf69
MYHITGITBIT <1120310228@hit.edu.cn>
2017-09-27 17:41:23 +0800

    Add 404 page and other Handler

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+from db import *
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('index.html')
-        #self.render('vesion.htm')
+        self.render('search.html', title = "Hello world")
 
 class VisionHandler(tornado.web.RequestHandler):
     def get(self):
@@ -23,14 +23,42 @@ class HeatMapHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('net.html')
         pass
+
 class SearchHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('search.html')
         pass
-class DataHandler(tornado.web.RequestHandler):
+
+class AboutHandler(tornado.web.RequestHandler):
     def get(self):
+        self.render('index.html')
+        pass
+
+class DataHandler(tornado.web.RequestHandler):
+    '''
+    def get(self):
+        query = self.get_argument("query")
+        print query
         self.render('data.html')
-    pass
+    '''
+
+    def post(self):
+        noun1 = self.get_argument('noun1')
+        db = DataBase()
+        print db.get_database('BioPA').name
+        result = db.search_item({'PathID': noun1})
+        if len(result) == 0:
+            self.render('error.html')
+        else:
+            self.render('data.html', noun1=result )
+
+class UserHandler(tornado.web.RequestHandler):
+    def post(self):
+        user_name = self.get_argument("username")
+        user_email = self.get_argument("email")
+        user_website = self.get_argument("website")
+        user_language = self.get_argument("language")
+        self.render("user.html",username=user_name,email=user_email,website=user_website,language=user_language)
 
 
 if __name__ == '__main__':
@@ -40,7 +68,9 @@ if __name__ == '__main__':
                   (r'/vesion', VisionHandler),
                   (r'/net', HeatMapHandler),
                   (r'/search', SearchHandler),
-                  (r'/data', DataHandler)],
+                  (r'/data', DataHandler),
+                  (r'/user', UserHandler),
+                  (r'/about', AboutHandler)],
 
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
