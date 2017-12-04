d2f6af5c3f4614983a64b807b1e086f45f00c08b
MYHITGITBIT <1120310228@hit.edu.cn>
2017-12-04 21:45:37 +0800

    Full screen of network board

--- handlers/test.py
+++ handlers/test.py
@@ -4,4 +4,4 @@ import tornado.web
 
 class TestHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('test2.html', title = "Hello world")
+        self.render('test.html', title = "Hello world")
